ef91b268cd14e4265a24c73fe458e5f776328c10
Pablo Menendez <pmenendez-2020332@kinal.edu.gt>
2025-04-07 17:12:18 -0600

    login finiquitado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useRoutes } from "react-router-dom"
-import { Toaster } from "react-hot-toast"
-import routes from "./routes.jsx"
+import { useRoutes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import routes from "./routes.jsx";
 
 
-export const App = () =>{
+export const App = () => {
 
   let element = useRoutes(routes);
+
   return (
     <>
-    {element}
-    <Toaster
-      position="bottom-right"
-      reverseOrder={false}
-    />
+      {element}
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+      />
     </>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,10 @@
-const routes = [
+import { DashboardPage } from './pages/dashboard';
+import { Auth } from './pages/auth';
 
+
+const routes = [
+    {path: '/auth', element: <Auth />},
+    {path: '/*', element: <DashboardPage />}
 ]
 
 export default routes
