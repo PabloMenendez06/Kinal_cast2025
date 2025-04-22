cb647fdfd5def5700223047d41e8853317cd3917
Pablo Menendez <pmenendez-2020332@kinal.edu.gt>
2025-04-22 16:52:12 -0600

    Proyecto Casi terminado

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -3,4 +3,6 @@ export { useRegister } from './useRegister'
 export { logout } from './userLogout'
 export { useUserDetails } from './useUserDetails'
 export {useChannelSettings} from './useChangeSettings'
-export {useChangePassword} from './useChangePassword'
+export { useChangePassword } from './useChangePassword'
+export { useChannelDetails } from './useChannelDetails';
+export { useFollowChannel } from './useFollowChannel'
